f6cdb734ef5cbe9fb86a17161e6b10842d7f792a
Ekaterina <katskktt@gmail.com>
2026-03-10 17:46:50 +0300

    ParameterizedTest commit

--- src/test/java/ru/netology/service/FreelancerServiceTest.java
+++ src/test/java/ru/netology/service/FreelancerServiceTest.java
@@ -2,6 +2,8 @@ package ru.netology.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import ru.netology.services.FreelancerService;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,8 +33,14 @@ class FreelancerServiceTest {
         int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
-
     }
 
-}
+    @ParameterizedTest
+    @CsvFileSource(resources = "test_data.csv")
+    public void testCalculateParams(int income, int expenses, int threshold, int expected) {
+        FreelancerService service = new FreelancerService();
+        int actual = service.calculate(income, expenses, threshold);
 
+        Assertions.assertEquals(expected, actual);
+    }
+}
