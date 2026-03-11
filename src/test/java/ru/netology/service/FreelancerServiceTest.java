69ec1b665b11a936f7dd923ccb1adf3e85738a02
Ekaterina <katskktt@gmail.com>
2026-03-11 17:09:13 +0300

    pom.xml added

--- src/test/java/ru/netology/service/FreelancerServiceTest.java
+++ src/test/java/ru/netology/service/FreelancerServiceTest.java
@@ -9,32 +9,6 @@ import ru.netology.services.FreelancerService;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FreelancerServiceTest {
-    @Test
-    public void testCalculateRestCase1() {
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
-
-        FreelancerService service = new FreelancerService();
-        int actual = service.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testCalculateRestCase2() {
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
-
-        FreelancerService service = new FreelancerService();
-        int actual = service.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
     @ParameterizedTest
     @CsvFileSource(resources = "test_data.csv")
     public void testCalculateParams(int income, int expenses, int threshold, int expected) {
